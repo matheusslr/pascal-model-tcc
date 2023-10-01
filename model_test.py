c2834a4238a4ae5a3663218e0f79b770073f0f5c
matheusslr <matheusslr@hotmail.com>
2023-10-01 08:31:29 -0300

    refactor: organizando o pipeline dos dados pelos fundamentos de ML

--- model_test.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.Sequential()
-model.add(tf.keras.Input(shape=(16,8)))
-model.add(tf.keras.layers.LSTM(2, activation="relu", dropout=0.2))
-model.add(tf.keras.layers.Dense(8))
-
-model.summary()
